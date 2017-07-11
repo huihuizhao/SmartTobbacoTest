2d2e47193fbd1888c30b4701bcbee5a79ddc26c1
COTTAGE\赵辉辉 <zhaohuihui@outlook.com>
2017-07-11 16:45:45 +0800

    获取经纬度  不稳定

--- app/src/main/java/com/esri/arcgisruntime/sample/smarttobacco/TransplantActivity.java
+++ app/src/main/java/com/esri/arcgisruntime/sample/smarttobacco/TransplantActivity.java
@@ -73,7 +73,7 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 public class TransplantActivity extends Activity implements OnItemClickListener {
-//    private TextView coordinatesTextView;
+    //    private TextView coordinatesTextView;
     //    private ImageView imageButton;
     private int width;
     private int height;
@@ -121,7 +121,7 @@ public class TransplantActivity extends Activity implements OnItemClickListener 
         setContentView(R.layout.activity_transplant);
 
 //        locationClient=((MyApplication)getApplication()).locationClient;
-        locationClient= new LocationClient(this);
+        locationClient = new LocationClient(this);
         locationClient.registerLocationListener(locListener);
 
         TextView mTitleView = (TextView) findViewById(R.id.title_text);
@@ -134,7 +134,7 @@ public class TransplantActivity extends Activity implements OnItemClickListener 
 
         textViewTransplantCoordinates = (TextView) findViewById(R.id.textViewTransplantCoordinates);
 //		String coordinares = GetCoordinates();
-        String coordinares = "0,0";
+        String coordinares = "0, 0";
         textViewTransplantCoordinates.setText(coordinares);
 
 
@@ -268,8 +268,6 @@ public class TransplantActivity extends Activity implements OnItemClickListener 
         locationClient.start();//默认发起1次请求
 
     }
-
-
 
 
     // 显示对话框
@@ -816,70 +814,124 @@ public class TransplantActivity extends Activity implements OnItemClickListener 
     }
 
 
-    private BDLocationListener locListener=new BDLocationListener()
-    {
-        StringBuilder sb=new StringBuilder();
+    private BDLocationListener locListener = new BDLocationListener() {
+        //        StringBuilder sb=new StringBuilder();
+
         @Override
         public void onReceiveLocation(BDLocation location) {
-            sb.append("当前时间 : ");
-            sb.append(location.getTime());
 
-            sb.append("\n定位类型 : ");
-            sb.append(location.getLocType());
+//            //获取定位结果
+//            StringBuffer sb = new StringBuffer(256);
+//
+//            sb.append("time : ");
+//            sb.append(location.getTime());    //获取定位时间
+//
+//            sb.append("\nerror code : ");
+//            sb.append(location.getLocType());    //获取类型类型
+//
+//            sb.append("\nlatitude : ");
+//            sb.append(location.getLatitude());    //获取纬度信息
+//
+//            sb.append("\nlontitude : ");
+//            sb.append(location.getLongitude());    //获取经度信息
+//
+//            sb.append("\nradius : ");
+//            sb.append(location.getRadius());    //获取定位精准度
+//
+//            if (location.getLocType() == BDLocation.TypeGpsLocation) {
+//
+//                // GPS定位结果
+//                sb.append("\nspeed : ");
+//                sb.append(location.getSpeed());    // 单位：公里每小时
+//
+//                sb.append("\nsatellite : ");
+//                sb.append(location.getSatelliteNumber());    //获取卫星数
+//
+//                sb.append("\nheight : ");
+//                sb.append(location.getAltitude());    //获取海拔高度信息，单位米
+//
+//                sb.append("\ndirection : ");
+//                sb.append(location.getDirection());    //获取方向信息，单位度
+//
+//                sb.append("\naddr : ");
+//                sb.append(location.getAddrStr());    //获取地址信息
+//
+//                sb.append("\ndescribe : ");
+//                sb.append("gps定位成功");
+//
+//            } else if (location.getLocType() == BDLocation.TypeNetWorkLocation) {
+//
+//                // 网络定位结果
+//                sb.append("\naddr : ");
+//                sb.append(location.getAddrStr());    //获取地址信息
+//
+//                sb.append("\noperationers : ");
+//                sb.append(location.getOperators());    //获取运营商信息
+//
+//                sb.append("\ndescribe : ");
+//                sb.append("网络定位成功");
+//
+//            } else if (location.getLocType() == BDLocation.TypeOffLineLocation) {
+//
+//                // 离线定位结果
+//                sb.append("\ndescribe : ");
+//                sb.append("离线定位成功，离线定位结果也是有效的");
+//
+//            } else if (location.getLocType() == BDLocation.TypeServerError) {
+//
+//                sb.append("\ndescribe : ");
+//                sb.append("服务端网络定位失败，可以反馈IMEI号和大体定位时间到loc-bugs@baidu.com，会有人追查原因");
+//
+//            } else if (location.getLocType() == BDLocation.TypeNetWorkException) {
+//
+//                sb.append("\ndescribe : ");
+//                sb.append("网络不同导致定位失败，请检查网络是否通畅");
+//
+//            } else if (location.getLocType() == BDLocation.TypeCriteriaException) {
+//
+//                sb.append("\ndescribe : ");
+//                sb.append("无法获取有效定位依据导致定位失败，一般是由于手机的原因，处于飞行模式下一般会造成这种结果，可以试着重启手机");
+//
+//            }
+//
+//            sb.append("\nlocationdescribe : ");
+//            sb.append(location.getLocationDescribe());    //位置语义化信息
+//
+//            List<Poi> list = location.getPoiList();    // POI数据
+//            if (list != null) {
+//                sb.append("\npoilist size = : ");
+//                sb.append(list.size());
+//                for (Poi p : list) {
+//                    sb.append("\npoi= : ");
+//                    sb.append(p.getId() + " " + p.getName() + " " + p.getRank());
+//                }
+//            }
+//
+//            Log.i("BaiduLocationApiDem", sb.toString());
 
-            sb.append("\n纬度 : ");
-            sb.append(location.getLatitude());
-            sb.append("\n精度 : ");
-            sb.append(location.getLongitude());
-            sb.append("\n定位半径 : ");
-            sb.append(location.getRadius());
-
-            if (location.getLocType() == BDLocation.TypeNetWorkLocation)
-            {
-                // 网络定位结果
-                sb.append("\n国 省 市 区.. : ");
-                sb.append(location.getAddrStr());
-                // 运营商信息
-                sb.append("\n网络定位结果 :");
-                sb.append("定位成功");
+            String coordinateInfo = location.getLongitude() + ", " + location.getLatitude();
+            if (coordinateInfo.length() > 10) {
+                textViewTransplantCoordinates.setText(location.getLongitude() + ", " + location.getLatitude());
             }
-            sb.append("\n简略地址信息 : ");
-            sb.append(location.getLocationDescribe());
 
-            List<Poi> list = location.getPoiList();// 附近信息
-            if (list != null)
-            {
-                sb.append("\npoilist size = : ");
-                sb.append(list.size());
-                for (Poi p : list)
-                {
-                    sb.append("\npoi= : ");
-                    sb.append(p.getId() + " " + p.getName() + " " + p.getRank());
-                }
-            }
-            Log.i("result:",sb.toString());
-//            result.setText(sb.toString());
 
-            textViewTransplantCoordinates.setText(location.getLongitude() +", "+location.getLatitude());
         }
 
-        public void onConnectHotSpotMessage(String s, int i){
+        public void onConnectHotSpotMessage(String s, int i) {
         }
 
     };
 
 
-    public void click(View v)
-    {
+    public void click(View v) {
         // send loc request
         initLoctionOption();
         locationClient.start();//默认发起1次请求
 
     }
 
-    private void initLoctionOption()
-    {
-        LocationClientOption locOption=new LocationClientOption();
+    private void initLoctionOption() {
+        LocationClientOption locOption = new LocationClientOption();
         //net loc
         locOption.setLocationMode(LocationClientOption.LocationMode.Battery_Saving);
         locOption.setCoorType("gcj02");// 定位结果坐标系
@@ -890,12 +942,12 @@ public class TransplantActivity extends Activity implements OnItemClickListener 
         locOption.setIgnoreKillProcess(true);
         locationClient.setLocOption(locOption);
     }
+
     @Override
     protected void onStop() {
         super.onStop();
         locationClient.stop();
     }
-
 
 
 }
